d8ea517a7d6bee9c9ce5ba02da4bdfefa0767b3f
rohan future devoloper <xrohaislam373@gmail.com>
2023-11-26 21:35:08 +0600

    fixed bug

--- src/Components/Container.jsx
+++ src/Components/Container.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
- 
 const Container = ({ children }) => {
-  return React.createElement("div" , {className : "mx-auto max-w-7xl"}, children);
+  return React.createElement(
+    "div",
+    { className: "mx-auto max-w-7xl" },
+    children
+  );
 };
 
 export default Container;
